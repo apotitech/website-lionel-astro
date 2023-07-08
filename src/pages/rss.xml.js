af475562643b86860a2b4dca08303add3141f2b4
apotinc <nfadalionel@gmail.com>
2023-07-07 21:22:37 -0600

    Editing Information

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,9 +5,9 @@ export const get = async () => {
   const blogs = await getCollection('blog', blog => blog.data.draft !== true);
 
   return rss({
-    title: 'Blog by Thomas Ledoux',
+    title: 'Blog by Lionel Tchami',
     description:
-      'Blog by Thomas Ledoux. Writes about Javascript/React/Next.js/Remix/Astro. Always looking to learn about the newest frameworks and features.',
+      'Blog by Lionel Tchami. Writes about DevOps/Python/AI/Cloud Tech/Astro. Always looking to learn about the newest technologies and features.',
     site: import.meta.env.SITE,
     items: blogs.map(post => ({
       link: `/blog/${post.slug}`,
